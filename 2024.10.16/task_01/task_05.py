50b3150cce557d884c6b461812e633dbe8b3c681
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-10-16 21:04:38 +0300

    изменение задачи под условие задачи 2024.10.16/task_05

--- 2024.10.16/task_01/task_05.py
+++ 2024.10.16/task_01/task_05.py
@@ -9,12 +9,11 @@ from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
 class RandomGenerator:
 
-    def __init__(self, n, use_uppercase=False, use_digits=False, use_punctuation=False, allow_duplicates=True):
+    def __init__(self, n, use_uppercase=False, use_digits=False, use_punctuation=False):
         self.n: int = n
         self.use_uppercase: bool = use_uppercase
         self.use_digits: bool = use_digits
         self.use_punctuation: bool = use_punctuation
-        self.allow_duplicates: bool = allow_duplicates
 
     def get_string(self) -> str:
         letters: str = ascii_lowercase
@@ -28,19 +27,7 @@ class RandomGenerator:
         if self.use_punctuation:
             letters += punctuation
 
-        result = list()
-
-        if not self.allow_duplicates:
-            letters_unique = set()
-            while len(letters_unique) < self.n:
-                let = random.choice(letters)
-                letters_unique.add(let)
-            result = list(letters_unique)
-
-        else:
-            result = [random.choice(letters) for _ in range(self.n)]
-
-        return ''.join(result)
+        return ''.join(random.choice(letters) for _ in range(self.n))
 
 
 if __name__ == '__main__':
@@ -51,6 +38,4 @@ if __name__ == '__main__':
     random_gen = RandomGenerator(10, use_uppercase=True, use_digits=True)
     print(random_gen.get_string())
     random_gen = RandomGenerator(10, use_uppercase=True, use_digits=True, use_punctuation=True)
-    print(random_gen.get_string())
-    random_gen = RandomGenerator(10, use_uppercase=True, use_digits=True, use_punctuation=True, allow_duplicates=True)
     print(random_gen.get_string())
