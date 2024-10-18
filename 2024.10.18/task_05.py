527eab2eb17aaeb3e91df42be0ce1c553bf22575
AlenaBorisovna <153104664+AlenaBorisovna@users.noreply.github.com>
2024-10-18 20:22:06 +0300

    решение задачи task_05

---
+++ 2024.10.18/task_05.py
@@ -0,0 +1,31 @@
+# Напишите программу, которая при запуске спрашивает
+# "На каком языке вы хотите цитату?" и даёт выбор: 0 - Русский | 1 - Английский
+# И в зависимости от выбора присылает цитату на выбранном языке.
+
+import requests
+
+
+def get_quote(lang):
+    url = 'http://api.forismatic.com/api/1.0/'
+    payload = {'method': 'getQuote', 'format': 'json', 'lang': lang}
+
+    res = requests.get(url, params=payload)
+    res.raise_for_status()
+    data = res.json()
+
+
+def get_lang():
+    lang = {0: 'ru', 1: 'en'}
+    choice = int(input("На каком языке вы хотите цитату?\n0 - Русский | 1 - Английский: "))
+    return lang.get(choice)
+
+
+if __name__ == '__main__':
+    url = 'http://api.forismatic.com/api/1.0/'
+
+    payload = {'method': 'getQuote', 'format': 'json', 'lang': get_lang()}
+
+    res = requests.get(url, params=payload)
+    data = res.json()
+
+    print(data)
